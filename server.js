aea7f986e2c23517127e75a8b291bd50b44286a3
oleg <codemorgan@gmail.com>
2025-07-22 08:24:33 +0300

    v2

---
+++ server.js
@@ -0,0 +1,19 @@
+
+// Import the built-in 'http' module
+const http = require('http');
+
+// Define the hostname and port
+const hostname = '0.0.0.0';
+const port = 4000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200; // Success status
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, your application version is v2.0.0!\n');
+});
+
+// Start listening on the defined hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
